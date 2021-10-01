c11f91fb001c10087ce1d0ae028fb941d0fc2ca6
SHAMS <shamsuddinpias0@gmail.com>
2021-10-01 21:00:18 +0600

    Basic bot aliases added

--- main.py
+++ main.py
@@ -1,76 +1,94 @@
-import os
 import discord
-from discord.ext import commands
+import os
+# load our local env so we dont have the token in public
 from dotenv import load_dotenv
-import youtube_dl
+from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from discord import TextChannel
+from youtube_dl import YoutubeDL
 
 load_dotenv()
-token = os.getenv("token")
+client = commands.Bot(command_prefix='.')  # prefix our commands with '.'
 
-client = commands.Bot(".")
+players = {}
 
 
-@client.command()
-async def play(ctx, url: str):
-    song_there = os.path.isfile("song.mp3")
-    try:
-        if song_there:
-            os.remove("song.mp3")
-    except PermissionError:
-        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+@client.event  # check if pommis is ready
+async def on_ready():
+    print('pommis online')
+
+
+# command for pommis to join the channel of the user,
+# if the pommis has already joined and is in a different channel,
+# it will move to the channel the user is in
+@client.command(aliases=["J", "j"])
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+
+# command to play sound from a youtube URL
+@client.command(aliases=["ply", "P", "p", "Ply", "PLy", "PLY", "pLY", "plY"])
+async def play(ctx, url):
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['url']
+        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice.is_playing()
+        await ctx.send('pommis is playing')
+
+    # check if the pommis is already playing
+    else:
+        await ctx.send("pommis is already playing")
         return
 
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='music')
-    await voiceChannel.connect()
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    for file in os.listdir("./"):
-        if file.endswith(".mp3"):
-            os.rename(file, "song.mp3")
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
-
-
-@client.command()
-async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send("The bot is not connected to a voice channel.")
-
-
-@client.command()
-async def pause(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_playing():
-        voice.pause()
-    else:
-        await ctx.send("Currently no audio is playing.")
-
-
+# command to resume voice if it is paused
 @client.command()
 async def resume(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_paused():
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
         voice.resume()
-    else:
-        await ctx.send("The audio is not paused.")
+        await ctx.send('pommis is resuming')
 
 
+# command to pause voice if it is playing
+@client.command()
+async def pause(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice.is_playing():
+        voice.pause()
+        await ctx.send('pommis has been paused')
+
+
+# command to stop voice
 @client.command()
 async def stop(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.stop()
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice.is_playing():
+        voice.stop()
+        await ctx.send('Stopping...')
 
 
-client.run(token)
+# command to clear channel messages
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send("Messages have been cleared")
+
+
+client.run(os.getenv('token'))
