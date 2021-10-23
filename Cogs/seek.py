15e02c4dd35423e8b94626de93ff74c4f7d98f04
SHAMS <shamsuddinpias0@gmail.com>
2021-10-23 19:57:48 +0600

    seek added

--- Cogs/seek.py
+++ Cogs/seek.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 
 from Tools.Check import Check
 
+import re
+
 
 class CogSeek(commands.Cog):
     def __init__(self, bot):
@@ -14,14 +16,23 @@ class CogSeek(commands.Cog):
                       description="Seek the current music.")
     @commands.guild_only()
     @commands.cooldown(1, 5, commands.BucketType.member)
-    async def seek(self, ctx):
+    async def seek(self, ctx, position: str):
 
         if not await Check().userInVoiceChannel(ctx, self.bot): return
         if not await Check().botInVoiceChannel(ctx, self.bot): return
         if not await Check().userAndBotInSameVoiceChannel(ctx, self.bot): return
 
+        TIME_REGEX = r"([0-9]{1,2})[:ms](([0-9]{1,2})s?)?"
+        if not (match := re.match(TIME_REGEX, position)):
+            await ctx.send(f"{ctx.author.mention} Please Provide valid time. Example: 10s")
+
+        if match.group(3):
+            secs = (int(match.group(1)) * 60) + (int(match.group(3)))
+        else:
+            secs = int(match.group(1))
+
         player = self.bot.wavelink.get_player(ctx.guild.id)
-        await player.seek(0)  # Reload the song
+        await player.seek(secs * 1000)  # seek the song with second
 
         await ctx.send(f"{ctx.author.mention} Current music Seeked!")
 
