f2e4a14bf0f195e76a8cf09f46c24f48d62bb069
SHAMS <shamsuddinpias0@gmail.com>
2021-10-23 23:33:20 +0600

    add lyrics

--- Cogs/lyrics.py
+++ Cogs/lyrics.py
@@ -6,6 +6,7 @@ from Tools.Check import Check
 import ksoftapi
 import json
 
+
 class CogLyrics(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -24,11 +25,11 @@ class CogLyrics(commands.Cog):
         if not await Check().botInVoiceChannel(ctx, self.bot): return
         if not await Check().userAndBotInSameVoiceChannel(ctx, self.bot): return
 
-        query = player.current.title.replace("*", "\\*")
-
         with open("configuration.json", "r") as config:
             data = json.load(config)
             apikey = data["lyricsApi"]
+
+        query = player.current.title.replace("*", "\\*")
 
         kclient = ksoftapi.Client(apikey)
 
@@ -38,6 +39,9 @@ class CogLyrics(commands.Cog):
             print('No lyrics found for ' + query)
         else:
             first = results[0]
+            print(first)
+            print("----------------")
+            print(first.lyrics)
             embed = discord.Embed(title=f"** [{query}] **", description=f"**[{first.lyrics}]**",
                                   color=discord.Colour.random())
             await ctx.send(embed=embed)
